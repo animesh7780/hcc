278c62b93fa208b5fc04c4daae31a955e4210958
animesh7780 <143063970+animesh7780@users.noreply.github.com>
2023-10-12 14:12:16 +0530

    hcc

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+
+    <h1>This is Admin Dashboard </h1>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/home',[HomeController::class,'redirect']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return redirect('/home'); // Redirect to the 'home' route
+})->name('dashboard');
+
