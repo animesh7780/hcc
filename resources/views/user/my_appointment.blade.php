152b838967d655ef512077d4ee2571496ac59ec7
itsdevraj002 <146914320+itsdevraj002@users.noreply.github.com>
2023-10-13 22:51:52 +0530

    Update my_appointment.blade.php

--- resources/views/user/my_appointment.blade.php
+++ resources/views/user/my_appointment.blade.php
@@ -131,7 +131,7 @@
               <td style= "padding:10px; color:white;">{{appoints->date}}</td>
               <td style= "padding:10px; color:white;">{{appoints->message}}</td>
               <td style= "padding:10px; color:white;">{{appoints->status}}</td>
-              <td><a class="bt btn-danger" href="{{url('cancel_appoint',$appoints->id)}}">Cancel </a></td>
+              <td><a class="bt btn-danger" onclick="return confirm('are you sure to delete this')" href="{{url('cancel_appoint',$appoints->id)}}">Cancel </a></td>
           </tr>
           
           @endforeach
