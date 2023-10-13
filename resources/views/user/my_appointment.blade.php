f465e68d73d91bd6b63ec9f41757eca0fc16b317
itsdevraj002 <146914320+itsdevraj002@users.noreply.github.com>
2023-10-13 22:20:19 +0530

    Update my_appointment.blade.php

--- resources/views/user/my_appointment.blade.php
+++ resources/views/user/my_appointment.blade.php
@@ -120,14 +120,21 @@
               <th style= "padding:10px;font-size:20px;color:white;">Date</th>
               <th style= "padding:10px;font-size:20px;color:white;">Message</th>
               <th style= "padding:10px;font-size:20px;color:white;">Status</th>
+
+              <th style= "padding:10px;font-size:20px;color:white;">Cancel Appointment</th>
           </tr>
 
-          <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+          @foreach($appoint as $appoints)
+
+          <tr style="background-color:black;" align="center">
+              <td style= "padding:10px; color:white;">{{appoints->doctor}}</td>
+              <td style= "padding:10px; color:white;">{{appoints->date}}</td>
+              <td style= "padding:10px; color:white;">{{appoints->message}}</td>
+              <td style= "padding:10px; color:white;">{{appoints->status}}</td>
+              <td><a class="bt btn-danger" href="{{url('cancel_appoint')}}">Cancel Appointment</a></td>
           </tr>
+          
+          @endforeach
 
           
       </table>
