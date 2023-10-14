6e562b474c07d46288a67073903b2db7c3bf2599
itsdevraj002 <146914320+itsdevraj002@users.noreply.github.com>
2023-10-15 00:33:08 +0530

    Update showappointment.blade.php

--- resources/views/admin/showappointment.blade.php
+++ resources/views/admin/showappointment.blade.php
@@ -1,10 +1,67 @@
+<x-app-layout>
+
+    <h1>This is Admin Dashboard </h1>
+</x-app-layout>
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title></title>
-</head>
-<body>
-    <h1>Test view</h1>
-</body>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include(admin.css)
+  </head>
+  <body>admin/
+    <div class="container-scroller">
+      <div class="row p-0 m-0 proBanner" id="proBanner">
+        <div class="col-md-12 p-0 m-0">
+          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+            <div class="ps-lg-1">
+              <div class="d-flex align-items-center justify-content-between">
+                <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
+                <a href="https://www.bootstrapdash.com/product/corona-free/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+              <a href="https://www.bootstrapdash.com/product/corona-free/"><i class="mdi mdi-home me-3 text-white"></i></a>
+              <button id="bannerClose" class="btn border-0 p-0">
+                <i class="mdi mdi-close text-white me-0"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.navbar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <div align="center" style="padding:100px;;">
+                <table>
+                    <tr style= "background-color:black;">
+                        <th style="padding:10px">Customer Name</th>
+                        <th style="padding:10px">Email</th>
+                        <th style="padding:10px">Phone</th>
+                        <th style="padding:10px">Doctor Name</th>
+                        <th style="padding:10px">Date</th>
+                        <th style="padding:10px">Message</th>
+                        <th style="padding:10px">Status</th>
+                    </tr>
+
+                    <tr align="center" style="background-color:skyblue;">
+
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
 </html>
