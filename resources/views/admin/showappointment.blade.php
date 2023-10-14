4e3463645f38c40475e5962aac8add5c77010221
itsdevraj002 <146914320+itsdevraj002@users.noreply.github.com>
2023-10-15 00:50:58 +0530

    Update showappointment.blade.php

--- resources/views/admin/showappointment.blade.php
+++ resources/views/admin/showappointment.blade.php
@@ -34,7 +34,7 @@
       @include('admin.navbar')
         <!-- partial -->
         <div class="container-fluid page-body-wrapper">
-            <div align="center" style="padding:100px;;">
+            <div align="center" style="padding-top:100px;">
                 <table>
                     <tr style= "background-color:black;">
                         <th style="padding:10px">Customer Name</th>
@@ -44,18 +44,27 @@
                         <th style="padding:10px">Date</th>
                         <th style="padding:10px">Message</th>
                         <th style="padding:10px">Status</th>
+                        <th style="padding:10px">Approved</th>
+                        <th style="padding:10px">Canceled</th>
                     </tr>
-
+                    @foreach($data as $appoint)
                     <tr align="center" style="background-color:skyblue;">
 
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{{$appoint->name}}</td>
+                        <td>{{$appoint->email}}</td>
+                        <td>{{$appoint->phone}}</td>
+                        <td>{{$appoint->doctor}}</td>
+                        <td>{{$appoint->date}}</td>
+                        <td>{{$appoint->message}}</td>
+                        <td>{{$appoint->status}}</td>
+                        <td>
+                            <a class="btn btn-success" href="{{url('approved,$appoint->id)}}">Approved</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-danger" href="">Canceled</a>
+                        </td>
                     </tr>
+                    @endforeach
                 </table>
             </div>
         </div>
