94aaf98d592e1ee6c4626152f54167b880e1a911
Raven113114 <147704270+Raven113114@users.noreply.github.com>
2023-10-16 20:35:12 +0530

    Update update_doctor.blade.php

--- resources/views/admin/update_doctor.blade.php
+++ resources/views/admin/update_doctor.blade.php
@@ -5,6 +5,18 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
+    <base href="/public">
+
+    <style type="text/css">
+        label
+        {
+            display: inline-block;
+
+            width: 200px;
+        }
+    </style>
+      
+        
     @include(admin.css)
   </head>
   <body>admin/
@@ -14,7 +26,46 @@
       <!-- partial -->
       @include('admin.navbar')
         <!-- partial -->
-       
+       <div class="container-fluid page-body-wrapper">
+           <div class="container" align="center" style="padding:100px">
+               <form action="{{url('editdoctor',$data->id)}}" method="POST" enctype="multipart/form-data"> 
+                   @csrf
+                   <div style="padding:15px;">
+                       <label>Doctor Name</label>
+                       <input type="text" style="color:black" name="name" value="{{$data->name}}">
+                   </div>
+                   <div style="padding:15px;">
+                       <label>Phone</label>
+                       <input type="number" style="color:black" name="name" value="{{$data->phone}}">
+                   </div>
+                   <div style="padding:15px;">
+                       <label>Speciality</label>
+                       <input type="text" style="color:black" name="name" value="{{$data->speciality}}">
+                   </div>
+                   <div style="padding:15px;">
+                       <label>Room</label>
+                       <input type="text" style="color:black" name="name" value="{{$data->room}}">
+                   </div>
+                   <div style="padding:15px;">
+                       <label>Old Image</label>
+                       <img height="150" width="150" src="doctorimage/{{$data->image}}">
+                   </div>
+                   <div style="padding:15px;">
+                       <label>Change Image</label>
+                       <input type="file" name="file">
+                   </div>
+                   <div style="padding:15px;">
+                      
+                       <input type="submit" class="btn btn-primary">
+                   </div>
+
+                   
+               </form>
+               
+           </div>
+
+       </div>
+        
     <!-- container-scroller -->
     <!-- plugins:js -->
     @include('admin.script')
