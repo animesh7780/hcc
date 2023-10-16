e3dfe62ebdd2e2787a865e7a4f6a726b1228241d
Raven113114 <147704270+Raven113114@users.noreply.github.com>
2023-10-16 19:48:44 +0530

    Update update_doctor.blade.php

--- resources/views/admin/update_doctor.blade.php
+++ resources/views/admin/update_doctor.blade.php
@@ -1,1 +1,24 @@
+<x-app-layout>
+    <h1>This is Admin Dashboard </h1>
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include(admin.css)
+  </head>
+  <body>admin/
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.navbar')
+        <!-- partial -->
+       
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
+</html>
 
